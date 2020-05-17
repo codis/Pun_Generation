a31d72acd716ce6aeee9e2755dba808733dcc26a
codis <costin.birlea@gmail.com>
2020-05-17 16:08:41 +0300

    Get best sentence with alternative word from list of puns

--- retrieve.py
+++ retrieve.py
@@ -1,6 +1,14 @@
+import pickle
+import random
 import sys
 import os
 import re
+
+def get_pun(pun_path):
+    with open(pun_path, 'rb') as f:
+        pair_puns = pickle.load(f)
+
+    return random.choice(pair_puns)
 
 def value(sentence):
     return 1
@@ -16,21 +24,15 @@ def possition_score(index, words):
 def eval(index, words):
     return length_score(words) + possition_score(index, words)
 
-def main():
-    filepath = sys.argv[1]
-    word = sys.argv[2]
-
-    if not os.path.isfile(filepath):
-        print("File path {} does not exist. Exiting...".format(filepath))
-        sys.exit()
-
-    with open(filepath) as fp:
+def get_sentence(sentence_path, pun):
+    (_, wa) = pun
+    with open(sentence_path) as fp:
         max_score = float('-inf')
         best_sentence = []
         for line in fp:
-            index = line.rfind(word)
+            index = line.rfind(wa)
             words = re.findall(r'\w+', line)
-            index = [i for i, x in enumerate(words) if word == x]
+            index = [i for i, x in enumerate(words) if wa == x]
 
             if len(index) != 0:
                 score = eval(index, words) * value(line)
@@ -39,7 +41,25 @@ def main():
                     max_score = score
                     best_sentence = line
 
-        return max_score, best_sentence
+    return max_score, best_sentence
+
+def main():
+    sentence_path = sys.argv[1]
+    pun_path = sys.argv[2]
+
+    if not os.path.isfile(sentence_path):
+        print("File path for Sentence data: {} does not exist. Exiting...".format(sentence_path))
+        sys.exit()
+
+    if not os.path.isfile(pun_path):
+        print("File path for Puns data: {} does not exist. Exiting...".format(pun_path))
+        sys.exit()
+
+    pun = get_pun(pun_path)
+    print(pun)
+
+    score, sentence = get_sentence(sentence_path, pun)
+    print(score, sentence)
 
 if __name__ == '__main__':
     print(main())
